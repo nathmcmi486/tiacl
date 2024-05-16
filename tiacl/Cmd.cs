744c43640a61a8e141669e0f0767a90c2ead0191
nathmcmi486 <nathmcmi486@ed.amdsb.ca>
2024-05-16 10:02:55 -0400

    Add project files.

---
+++ tiacl/Cmd.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiacl
+{
+    public class Cmd
+    {
+        public Cmd()
+        {
+
+        }
+
+        public const String HELP1 = "--help";
+        public const String HELP2 = "-h";
+        public const String INTERPRET = "-i";
+        public const String COMPILE = "-c";
+
+        public bool interpreting = false;
+        public bool compiling = false;
+        public String fileName;
+
+        void displayHelp()
+        {
+            Console.WriteLine("\nTIACL command options:");
+            Console.WriteLine($"{HELP1} or {HELP2}:\n\tDisplays this message");
+            Console.WriteLine($"{INTERPRET} <file_name>.tic\n\tStart interpreting program");
+            Console.WriteLine($"{COMPILE} <file_name>.tic\n\tStart compiling program");
+            Console.WriteLine("\n");
+        }
+
+        bool checkFile(String file)
+        {
+            if (file.Contains(".tic"))
+            {
+                bool exists = File.Exists(file);
+                if (exists)
+                {
+                    fileName = file;
+                    return true;
+                } else
+                {
+                    Console.WriteLine($"{file} is an invalid file");
+                    Environment.Exit(-1);
+                    return false;
+                }
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void runCMdArgs(String[] args)
+        {
+            switch (args[0])
+            {
+                case HELP1: case HELP2:
+                    displayHelp();
+                    break;
+                case INTERPRET:
+                    checkFile(args[1]);
+                    interpreting = true;
+                    break;
+                case COMPILE:
+                    checkFile(args[1]);
+                    compiling = true;
+                    break;
+                default:
+                    Console.WriteLine($"{args[0]} is invalid");
+                    break;
+            }
+        }
+    }
+}
---
+++ tiacl/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace tiacl
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(String[] args)
+        {
+            Cmd cmd = new Cmd();
+            cmd.runCMdArgs(args);
+
+            if (cmd.interpreting == true)
+            {
+                Console.WriteLine($"Interpreting {cmd.fileName}...");
+            } else if (cmd.compiling == true)
+            {
+                Console.WriteLine($"Compiling {cmd.fileName}...");
+            }
+        }
+    }
+}
